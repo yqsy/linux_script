8738368e95f0b47b64433b1f0538b0c536ab74f9
yqsy <yqsy021@126.com>
2017-02-05 22:06:03 +0800

    first commit

---
+++ centos7.sh
@@ -0,0 +1,11 @@
+yum update -y
+yum install -y vim
+yum install -y net-tools
+LANG="en_US.utf8"
+
+wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
+wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.9.1-1.el7.centos.x86_64.rpm
+yum -y install docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
+yum -y install docker-engine-1.9.1-1.el7.centos.x86_64.rpm
+service docker start
+
