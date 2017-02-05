ba3e584bc9f1a15e9dabe23a317fda8fb9fe8a8a
yqsy <yqsy021@126.com>
2017-02-05 22:18:51 +0800

    rm rpm

--- centos7.sh
+++ centos7.sh
@@ -7,5 +7,8 @@ wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-sel
 wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.9.1-1.el7.centos.x86_64.rpm
 yum -y install docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
 yum -y install docker-engine-1.9.1-1.el7.centos.x86_64.rpm
+rm -rf docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
+rm -rf docker-engine-1.9.1-1.el7.centos.x86_64.rpm
+
 service docker start
 
