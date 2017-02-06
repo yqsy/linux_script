80955761f0e217202e4482f9a4301e335945a01c
yqsy <yqsy021@126.com>
2017-02-06 23:51:12 +0800

    yum -y install docker docker-registry

--- centos7.sh
+++ centos7.sh
@@ -8,14 +8,9 @@ LANG="en_US.utf8"
 
 # docker
 if ! rpm -qa | grep docker-engine; then
-    wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
-    wget https://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-1.9.1-1.el7.centos.x86_64.rpm
-    yum -y install docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
-    yum -y install docker-engine-1.9.1-1.el7.centos.x86_64.rpm
-    rm -rf docker-engine-selinux-1.9.1-1.el7.centos.noarch.rpm
-    rm -rf docker-engine-1.9.1-1.el7.centos.x86_64.rpm
+    yum -y install docker docker-registry
 fi
-service docker start
+systemctl enable docker.service
 
 
 ## ~/.vimrc
