063c84de5320a4f7415381d01383464d23cd3b05
Guochun.Gong <yqsy021@126.com>
2017-02-20 16:47:04 +0800

    设置环境变量需要export

--- centos7.sh
+++ centos7.sh
@@ -26,8 +26,10 @@ yum install -y gcc
 yum install -y epel-release
 yum install -y iperf3
 yum install -y augeas
-LANG="en_US.utf8"
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 
 # docker
 # https://docs.docker.com/datacenter/ucp/1.1/installation/system-requirements/
