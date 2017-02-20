86d0bb9ba2c88eafd9b76dd578c46118a9368759
Guochun.Gong <yqsy021@126.com>
2017-02-20 18:20:07 +0800

    update utf-8

--- centos7.sh
+++ centos7.sh
@@ -30,6 +30,8 @@ yum install -y augeas
 localectl set-locale LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+echo export LC_ALL=en_US.UTF-8 >> ~/.bashrc
 
 # docker
 # https://docs.docker.com/datacenter/ucp/1.1/installation/system-requirements/
